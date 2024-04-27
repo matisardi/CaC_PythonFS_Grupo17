53069401ba3e7f3ccb3bcba3f208048d8b99c541
matisardi <matisardi@hotmail.com>
2024-04-27 18:05:16 -0300

    Agrego precarga de imágenes y comentarios

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
-let images = [
-    '../img/people-stretching-park.jpg',
+let images = [          //imágenes para el fondo, la última está en el css y se ve primera
     '../img/football-trainer-teaching-his-pupils.jpg',
     '../img/young-sportive-couple-doing-yoga-fitness-people-summer-park.jpg',
-    '../img/man-woman-running-track-side-view.jpg'
+    '../img/man-woman-running-track-side-view.jpg',
+    '../img/people-stretching-park.jpg'
 ];
 const bgMain = document.getElementById("imgMain");
-let intervaloFondo;
-let img = 0;
+let intervalo;
+let img;
 
 function cambiaFondo() {
-    bgMain.style.backgroundImage = "url(" + images[img] + ")";
-    // bgMain.style = {
-    //     backgroundImage : "url(" + images[img] + ")"
-    // }
+    bgMain.style.backgroundImage = "url(" + images[img] + ")";  //cambia imagen de fondo
     img++;
     if (img >= images.length) img = 0;
 }
 
-intervaloFondo = setInterval(cambiaFondo, 5000);
+for (img = 0; img < images.length; img++) {                     //precarga imágenes
+    bgMain.style.backgroundImage = "url(" + images[img] + ")";
+}
 
-console.log("hola");
+img = 0;
+intervalo = setInterval(cambiaFondo, 5000);                     //llama función cambiaFondo luego de 5 seg
