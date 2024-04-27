138b0efc08631d453ce4923dcedcd22b8816f5dd
matisardi <matisardi@hotmail.com>
2024-04-27 18:20:49 -0300

    Corrección de errores con imágenes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 let images = [          //imágenes para el fondo, la última está en el css y se ve primera
-    '../img/football-trainer-teaching-his-pupils.jpg',
-    '../img/young-sportive-couple-doing-yoga-fitness-people-summer-park.jpg',
-    '../img/man-woman-running-track-side-view.jpg',
-    '../img/people-stretching-park.jpg'
+    '../img/people-stretching-park_modif.jpg',
+    '../img/football-trainer-teaching-his-pupils_modif.jpg',
+    '../img/young-sportive-couple-doing-yoga-fitness-people-summer-park_modif.jpg',
+    '../img/man-woman-running-track-side-view_modif.jpg'
 ];
 const bgMain = document.getElementById("imgMain");
 let intervalo;
@@ -14,9 +14,9 @@ function cambiaFondo() {
     if (img >= images.length) img = 0;
 }
 
-for (img = 0; img < images.length; img++) {                     //precarga imágenes
-    bgMain.style.backgroundImage = "url(" + images[img] + ")";
-}
+// for (img = 0; img < images.length; img++) {                     //precarga imágenes
+//     bgMain.style.backgroundImage = "url(" + images[img] + ")";
+// }
 
 img = 0;
 intervalo = setInterval(cambiaFondo, 5000);                     //llama función cambiaFondo luego de 5 seg
