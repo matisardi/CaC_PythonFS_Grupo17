fc690970fd174e7f422e6524e0f952ba54c30f27
m.veronica prola <verot30@gmail.com>
2024-05-02 16:50:27 -0300

    diseñologo-estilos-colore-tipografia-responsive-grilla

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 let images = [          //imágenes para el fondo, la última está en el css y se ve primera
-    "./img/people-stretching-park_modif.jpg",
-    "./img/football-trainer-teaching-his-pupils_modif.jpg",
-    "./img/young-sportive-couple-doing-yoga-fitness-people-summer-park_modif.jpg",
-    "./img/man-woman-running-track-side-view_modif.jpg"
+    "/img/sports-roller-00.jpg",
+    "/img/sports-ride-bike-00.jpg",
+    "/img/sport-football-trainer-00.jpg",
+    "/img/sport-running-00.jpg"
 ];
 const bodyBefore = document.styleSheets[0].cssRules[1];
 const bgMain = document.getElementById("imgMain");
