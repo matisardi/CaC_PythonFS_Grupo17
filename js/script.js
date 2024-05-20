f78610fdbec9a0d8834f7849147d0d45219f2b34
matisardi <matisardi@hotmail.com>
2024-05-20 09:08:17 -0300

    Agrego frases a imágenes rotativas

--- js/script.js
+++ js/script.js
@@ -4,14 +4,29 @@ let images = [          //imágenes para el fondo, la última está en el css y 
     "./img/sport-football-trainer-00.jpg",
     "./img/sport-running-00.jpg"
 ];
+let frases = [
+    "... y en que lugares practicarlos",
+    "Disfruta de un momento con amigos...",
+    "... mientras realizas una actividad saludable!",
+    "Entérate de los deportes que puedes hacer al aire libre..."
+];
+let fraseColor = [
+    "black",
+    "white",
+    "white",
+    "black"
+];
 const bgMain = document.querySelector('section');
+const fraseActual = document.querySelector('section p');
 let intervalo;
-let img = 0;
+let cambio = 0;
 
 function cambiaFondo() {
-    bgMain.style.backgroundImage = "url(" + images[img] + ")";  //cambia imagen de fondo
-    img++;
-    if (img >= images.length) img = 0;
+    bgMain.style.backgroundImage = "url(" + images[cambio] + ")";  //cambia imagen de fondo
+    fraseActual.innerHTML = frases[cambio];
+    fraseActual.style.color = fraseColor[cambio];
+    cambio++;
+    if (cambio >= images.length) cambio = 0;
 }
 
 intervalo = setInterval(cambiaFondo, 5000);   //llama función cambiaFondo luego de 5 seg
